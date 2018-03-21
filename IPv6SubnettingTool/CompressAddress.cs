35cdea039d81951eaf533af011bb4559c3071790
ygvn <yucelguven@hotmail.com>
2018-03-21 23:46:13 +0200

    2nd commit - 20180321 - Yucel Guven

---
+++ IPv6SubnettingTool/CompressAddress.cs
@@ -0,0 +1,134 @@
+﻿/*
+ * Copyright (c) 2010-2018 Yucel Guven
+ * All rights reserved.
+ * 
+ * This file is part of IPv6 Subnetting Tool.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS 
+ * AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Globalization;
+
+namespace IPv6SubnettingTool
+{
+    public partial class CompressAddress : Form
+    {
+        v6ST v6st = new v6ST();
+        SEaddress seaddress = new SEaddress();
+        public CultureInfo culture;
+        public delegate void ChangeWinFormStringsDelegate(CultureInfo culture);
+        public event ChangeWinFormStringsDelegate ChangeUILanguage = delegate { };
+
+        public CompressAddress(CultureInfo culture)
+        {
+            InitializeComponent();
+            this.culture = culture;
+            this.SwitchLanguage(this.culture);
+        }
+
+        public void SwitchLanguage(CultureInfo culture)
+        {
+            this.culture = culture;
+            this.Text = StringsDictionary.KeyValue("CompressForm.Text", this.culture);
+            this.label1.Text = StringsDictionary.KeyValue("CompressForm_label1.Text", this.culture);
+            this.label2.Text = StringsDictionary.KeyValue("CompressForm_label2.Text", this.culture);
+            this.label4.Text = StringsDictionary.KeyValue("CompressForm_label4.Text", this.culture);
+            this.button1.Text = StringsDictionary.KeyValue("CompressForm_button1.Text", this.culture);
+            //
+            this.ChangeUILanguage.Invoke(this.culture);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            textBox1.Text = textBox1.Text.Trim();
+            this.Calculate(textBox1.Text);
+        }
+
+        private void Calculate(string sin)
+        {
+            if (v6st.IsAddressCorrect(textBox1.Text))
+            {
+                label3.Text = StringsDictionary.KeyValue("Form1_" + v6ST.errmsg, this.culture);
+                string Resv6 = v6st.FormalizeAddr(sin);
+                
+                // :-) full
+                string veryformal = v6st.Kolonlar(Resv6, CheckState.Checked);
+                
+                textBox2.Text = v6st.CompressAddress(veryformal);
+                
+                string[] formal = veryformal.Split(':');
+                textBox3.Clear();
+                foreach (string s in formal)
+                    textBox3.Text +=
+                        UInt16.Parse(s, System.Globalization.NumberStyles.AllowHexSpecifier).ToString("x")
+                        + ":";
+                textBox3.Text = textBox3.Text.TrimEnd(':');
+
+                seaddress.Resultv6 = seaddress.Start =
+                    BigInteger.Parse("0" + Resv6, NumberStyles.AllowHexSpecifier);
+                textBox4.Text = v6st.PrintBin(seaddress, 128, CheckState.Checked).Replace(':', ' ');
+                textBox5.Text = "0x" + Resv6;
+                textBox6.Text = v6st.DnsRev(seaddress.Resultv6, 128, CheckState.Checked)[0];
+                textBox7.Text = v6st.AddressType(seaddress.Resultv6);
+                textBox8.Text = seaddress.Resultv6.ToString();
+                textBox9.Text = veryformal;
+            }
+            else
+            {
+                label3.Text = StringsDictionary.KeyValue("Form1_" + v6ST.errmsg, this.culture);
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+                textBox6.Clear();
+                textBox7.Clear();
+                textBox8.Clear();
+                textBox9.Clear();
+            }
+        }
+
+        private void CompressAddressForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                IPv6SubnettingTool.Form1.RemoveForm(this.GetHashCode());
+                this.Close();
+            }
+        }
+
+        private void CompressAddressForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            IPv6SubnettingTool.Form1.RemoveForm(this.GetHashCode());
+        }
+    }
+}
---
+++ IPv6SubnettingTool/PrefixSubLevels.cs
@@ -0,0 +1,314 @@
+﻿/*
+ * Copyright (c) 2010-2018 Yucel Guven
+ * All rights reserved.
+ * 
+ * This file is part of IPv6 Subnetting Tool.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS 
+ * AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Data.Odbc;
+using System.Numerics;
+
+namespace IPv6SubnettingTool
+{
+    public partial class PrefixSubLevels : Form
+    {
+        #region special initials/constants -yucel
+        string prefix = null;
+        short pflen = 0;
+        string parentprefix = null;
+        short parentpflen = 0;
+        string end = null;
+        CheckState chks = CheckState.Unchecked;
+        int t1 = 0, t2 = 0;
+        List<string[]> liste = new List<string[]>();
+
+        v6ST v6st = new v6ST();
+        OdbcConnection MySQLconnection;
+        DBServerInfo ServerInfo = new DBServerInfo();
+        OdbcDataReader MyDataReader;
+        public CultureInfo culture;
+        public delegate void ChangeWinFormStringsDelegate(CultureInfo culture);
+        public event ChangeWinFormStringsDelegate ChangeUILanguage = delegate { };
+
+        TreeNode tnode = new TreeNode();
+        TreeNode root = new TreeNode();
+        private const string plus = "plus";
+        #endregion
+
+        public PrefixSubLevels(string prefix, short pflen, string parentprefix, CheckState chks, 
+            int t1, int t2, OdbcConnection sqlcon, DBServerInfo servinfo, CultureInfo culture)
+        {
+            InitializeComponent();
+
+            this.prefix = this.v6st.CompressAddress(prefix);
+            this.parentprefix = parentprefix;
+            this.pflen = pflen;
+            this.parentpflen = pflen;
+            this.chks = chks;
+            this.t1 = t1; this.t2 = t2;
+            this.end = this.FindEnd(this.prefix, this.pflen, this.chks);
+            this.MySQLconnection = sqlcon;
+            this.ServerInfo = servinfo;
+            this.culture = culture;
+            this.SwitchLanguage(this.culture);
+
+            this.textBox2.ForeColor = Color.FromKnownColor(KnownColor.RoyalBlue);
+            this.textBox2.BackColor = Color.FromKnownColor(KnownColor.Control);
+
+            this.textBox1.Text = this.parentprefix;
+            this.textBox2.Text = "└ " + this.prefix + "/" + this.pflen;
+
+            this.treeView1.Nodes.Clear();
+            this.root = this.treeView1.Nodes.Add(this.prefix + "/" + this.pflen);
+            this.treeView1.Nodes[0].Nodes.Add(plus);
+
+
+            if (this.MySQLconnection == null)
+                this.label4.Text = "db=DOWN";
+            else
+            {
+                if (this.MySQLconnection.State == ConnectionState.Open)
+                    this.label4.Text = "db=UP";
+                else if (this.MySQLconnection.State == ConnectionState.Closed)
+                    this.label4.Text = "db=DOWN";
+            }
+        }
+
+        private string FindEnd(string snet, short pflen, CheckState chks) 
+        {
+            SEaddress se = new SEaddress();
+            string end = "";
+            string start = v6st.FormalizeAddr(snet);
+            if (this.chks == CheckState.Checked) /* 128 bits */
+            {
+                if (start.Length == 32)
+                    start = "0" + start;
+                se.Start = BigInteger.Parse(start, NumberStyles.AllowHexSpecifier);
+                se.slash = this.t1;
+                se.subnetslash = pflen;
+                se = v6st.Subnetting(se, this.chks);
+                end = String.Format("{0:x}", se.End);
+                if (end.Length > 32)
+                    end = end.Substring(1, 32);
+                end = v6st.Kolonlar(end, this.chks);
+            }
+            else if (this.chks == CheckState.Unchecked) /* 64 bits */
+            {
+                if (start.Length == 16)
+                    start = "0" + start;
+                start = start.Substring(0, 16);
+                se.Start = BigInteger.Parse(start, NumberStyles.AllowHexSpecifier);
+                se.slash = this.t1;
+                se.subnetslash = pflen;
+                se = v6st.Subnetting(se, this.chks);
+                end = String.Format("{0:x}", se.End);
+                if (end.Length > 16)
+                    end = end.Substring(1, 16);
+                end = v6st.Kolonlar(end, this.chks);
+            }
+            end = this.v6st.CompressAddress(end);
+            
+            return end;
+        }
+
+        public int MySQLquery(string inprefix, string end, short pflen)
+        {
+            if (this.MySQLconnection == null)
+                return -1;
+
+            if (this.MySQLconnection.State == ConnectionState.Closed)
+            {
+                MessageBox.Show(StringsDictionary.KeyValue("FormDB_MySQLquery_closed", this.culture),
+                    StringsDictionary.KeyValue("FormDB_MySQLquery_closed_header", this.culture),
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return -1;
+            }
+
+            this.liste.Clear();
+
+            int r = 0;
+            string MySQLcmd = "";
+            string is128bits = "";
+            
+            if (this.chks == CheckState.Unchecked)
+            {
+                is128bits = " AND pflen > " + pflen + " AND pflen <= 64";
+            }
+            else if (this.chks == CheckState.Checked)
+            {
+                is128bits = " AND pflen > " + pflen;
+            }
+
+            MySQLcmd = "SELECT inet6_ntoa(prefix), pflen, netname, status "
+                + " from " + this.ServerInfo.Tablename
+                + " WHERE ( prefix BETWEEN inet6_aton('" + inprefix + "') "
+                + " AND inet6_aton('" + end + "') "
+                + is128bits + " AND parentpflen= " + pflen + ") "
+                + " LIMIT 32768; ";
+            
+            OdbcCommand MyCommand = new OdbcCommand(MySQLcmd, this.MySQLconnection);
+
+            try
+            {
+                MyDataReader = MyCommand.ExecuteReader();
+
+                r = MyDataReader.RecordsAffected;
+                this.liste.Clear();
+
+                while (MyDataReader.Read())
+                {
+                    this.liste.Add(new string[]{
+                        MyDataReader.GetString(0), MyDataReader.GetByte(1).ToString(),
+                        MyDataReader.GetString(2),
+                        MyDataReader.GetString(3)}
+                        );
+                }
+
+                MyDataReader.Close();
+                if (MyDataReader is IDisposable)
+                    MyDataReader.Dispose();
+                return r;
+            }
+            //catch (System.InvalidOperationException ex)
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message +
+                    StringsDictionary.KeyValue("FormDB_MySQLquery_exception", this.culture),
+                    StringsDictionary.KeyValue("FormDB_MySQLquery_exception_header", this.culture),
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return -1;
+            }
+        }
+
+        public void DBStateChange(OdbcConnection dbconn, DBServerInfo servinfo)
+        {
+            this.MySQLconnection = dbconn;
+            this.ServerInfo = servinfo;
+
+            if (this.MySQLconnection == null)
+                this.label4.Text = "db=DOWN";
+            else
+            {
+                if (this.MySQLconnection.State == ConnectionState.Open)
+                    this.label4.Text = "db=UP";
+                else if (this.MySQLconnection.State == ConnectionState.Closed)
+                    this.label4.Text = "db=DOWN";
+            }
+
+            if (this.MySQLconnection != null)
+                return;
+            else
+            {
+                IPv6SubnettingTool.Form1.RemoveForm(this.GetHashCode());
+
+                if (this is IDisposable)
+                    this.Dispose();
+                else
+                    this.Close();
+            }
+        }
+
+        private void AddNode(TreeNode node)
+        {
+            string inprefix = node.Text.Split('/')[0];
+            short pflen = Convert.ToInt16(node.Text.Split('/')[1]);
+            string end = this.FindEnd(inprefix, pflen, this.chks);
+
+            this.treeView1.BeginUpdate();
+            int r = this.MySQLquery(inprefix, end, pflen);
+
+            if (r > 0)
+            {
+                int i = 0;
+                foreach (string[] s in this.liste)
+                {
+                    node.Nodes.Add(s[0] + "/" + s[1]);
+
+                    if (this.chks == CheckState.Unchecked)
+                    {
+                        if (s[1] != "64")
+                            node.Nodes[i].Nodes.Add(plus);
+                    }
+                    else if (this.chks == CheckState.Checked)
+                    {
+                        if (s[1] != "128")
+                            node.Nodes[i].Nodes.Add(plus);
+                    }
+                    i++;
+                }
+            }
+            this.treeView1.EndUpdate();
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            this.AddNode(e.Node);
+        }
+
+        private void treeView1_BeforeCollapse(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            e.Node.Nodes.Add(plus);
+        }
+
+        private void PrefixSubLevelsForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                IPv6SubnettingTool.Form1.RemoveForm(this.GetHashCode());
+                this.Close();
+            }
+        }
+
+        public void SwitchLanguage(CultureInfo culture)
+        {
+            this.culture = culture;
+            this.Text = StringsDictionary.KeyValue("PrefixSubLevelsForm.Text", this.culture);
+            //
+            this.label1.Text = StringsDictionary.KeyValue("PrefixSubLevelsForm_label1.Text", this.culture);
+            this.label2.Text = StringsDictionary.KeyValue("PrefixSubLevelsForm_label2.Text", this.culture);
+            this.label3.Text = StringsDictionary.KeyValue("PrefixSubLevelsForm_recordlimit", this.culture);
+
+            this.ChangeUILanguage.Invoke(this.culture);
+        }
+
+        private void PrefixSubLevelsForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            IPv6SubnettingTool.Form1.RemoveForm(this.GetHashCode());
+        }
+
+    }
+}
